0ac2fe77fa9b168b583440d4b9d292d010ffc85c
KARTHIKTHILAK <karthikthilak2010@gmail.com>
2024-12-12 21:45:26 +0530

    Hello to Python

---
+++ hello.py
@@ -0,0 +1,27 @@
+hi = "Hello, World"
+print(hi)                       # Hello World
+
+
+line1 = "***************************"
+line2 = "                           "
+line3 = "          Karthik          "
+
+print(line1)
+print(line2)
+print(line3)
+print(line2)
+print(line1)
+
+
+
+a = 9
+
+if a>5:
+    print("Hi")
+else:
+    print("Welcome")
+
+
+# TERNARY OPERATORS
+
+print("Hi") if a>4 else print("Welcome")
