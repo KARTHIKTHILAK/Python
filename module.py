84eb7687e829e65c11acc1fed1da7fe8b15a45fd
KARTHIKTHILAK <karthikthilak2010@gmail.com>
2024-12-15 21:16:35 +0530

    Module in Python with My own RPS game library was Added

---
+++ module.py
@@ -0,0 +1,14 @@
+import math
+from math import pi
+print(pi,"\n")
+
+import random as rdm
+print(dir(rdm))
+
+for item in dir(rdm):
+    print(item)
+
+import rps7pro
+
+print(__name__)
+print(rps7pro.play())
