8cc04f151fe96d312de632bd10e69e3636883b1e
KARTHIKTHILAK <karthikthilak2010@gmail.com>
2024-12-15 20:33:24 +0530

    f-string in Python

---
+++ fstring.py
@@ -0,0 +1,69 @@
+
+# f-string
+
+person = "Karthik"
+coins = 3
+
+###########################
+# Concatenating string
+
+print("\n" + person + " has " + str(coins) + " coins left.")
+
+
+###########################
+# Previous %s formatting
+
+message = "\n%s has %s coins left." % (person,coins)
+print(message)
+
+###########################
+# Previous string.format() method
+
+message = "\n{} has {} coins left.".format(person,coins)
+print(message)
+
+message = "\n{1} has {0} coins left.".format(coins,person)
+print(message)
+
+message = "\n{0} has {1} coins left.".format(coins,person)
+print(message)
+
+message = "\n{person} has {coins} coins left.".format(
+    coins = coins , person = person
+)
+print(message)
+
+player = {'person' : 'Karthik' , 'coins' : 3 }
+
+message = "\n{person} has {coins} coins left.".format(**player)
+
+
+###############################################
+# f-Strings! This is the way.
+print(" ")
+
+print("# This is the f-string : ")
+
+message = f"\n{person} has {coins} coins left."
+print(message)
+
+message = f"\n{person} has {2*5} coins left."
+print(message)
+
+message = f"\n{person.lower()} has {2*5} coins left."
+print(message)
+
+message = f"\n{player['person']} has {2*5} coins left."
+print(message)
+
+###############################
+# You can pass formatting options
+
+num = 10
+print(f"\n2.25 times {num} is {2.25 * num:.2f}\n")
+
+for num in range(1,11):
+    print(f"2.25 times {num} is {2.25*num:.2f}")
+
+for num in range(1,11):
+    print(f"{num} divided by 4.52 is {num/4.52:.2%}")
