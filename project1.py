ca96f8a8987a2492e6be41e89d1757cf09d08b5d
KARTHIKTHILAK <karthikthilak2010@gmail.com>
2024-12-15 18:16:46 +0530

    Rock , Paper , Scissor Game in Python was Added with Extra Features

--- project1.py
+++ project1.py
@@ -9,78 +9,100 @@ class RPS(Enum):
     PAPER = 2
     SCISSOR = 3
 
-played_game_count = 0
+def rps():
 
-def rps_game():
-    
-    print(" ")
-    
-    print("Welcome to ROCK , PAPER , SCISSOR Game : ")
-    
-    print(" ")
-    
-    Player1_choice = input("Enter....\n1 for Rock,\n2 for Paper,\n3 for Scissor :\n\n")    
-    Player1_value = int(Player1_choice)
+    played_game_count = 0
+    python_wins = 0
+    player_wins = 0
 
-    if Player1_choice not in ["1","2","3"]:
-        print("You must enter 1, 2, or 3.")
-        return rps_game()
+    def rps_game():
         
-    Computer_choice = random.choice("123") 
-    Computer_value = int(Computer_choice) 
-    
-    print("") 
-    
-    print("You Choose : " + Player1_choice + " " + str(RPS(Player1_value)).replace("RPS.", " ") + ".")
-    print("Python Choose : " + Computer_choice + " " + str(RPS(Computer_value)).replace("RPS.", " ") + ".")
-    
-    print("") 
-    
-    # Game Playing Rules
-    
-    if Player1_value == Computer_value: 
-        print("😐Draw Match😐")
-    elif Player1_value == 1 and Computer_value == 3: 
-        print("🥇Won by Player1🏆") 
-    elif Player1_value == 2 and Computer_value == 1: 
-        print("🥇Won by Player1🏆")
-    elif Player1_value == 3 and Computer_value == 2:
-        print("🥇Won by Player1🏆")
-    else:
-        print("🐍Won by Python🐍") 
-    
-    print("")
-    
-    global played_game_count
-    played_game_count += 1
-    print("Game Played Count : " + str(played_game_count))
-
-    print(" ")
-    
-    print("Play again ? \nEnter y for Play again \nEnter q for Quit ")
-    
-    print(" ")
-
-    while True:
+        print(" ")
         
-        last_value = input("y/q : ")
+        print("Welcome to ROCK , PAPER , SCISSOR Game : ")
+        
+        print(" ")
+        
+        Player1_choice = input("Enter....\n1 for Rock,\n2 for Paper,\n3 for Scissor :\n\n")    
+        Player1_value = int(Player1_choice)
+
+        if Player1_choice not in ["1","2","3"]:
+            print("You must enter 1, 2, or 3.")
+            return rps_game()
+            
+        Computer_choice = random.choice("123") 
+        Computer_value = int(Computer_choice) 
+        
+        print("") 
+        
+        print("You Choose : " + Player1_choice + " " + str(RPS(Player1_value)).replace("RPS.", " ") + ".")
+        print("Python Choose : " + Computer_choice + " " + str(RPS(Computer_value)).replace("RPS.", " ") + ".")
+        
+        print("") 
+        
+        # Game Playing Rules
+        
+        def decide_winner(Player1_value,Computer_value):
+
+            nonlocal player_wins
+            nonlocal python_wins
+                
+            if Player1_value == Computer_value: 
+                return "😐Draw Match😐"
+            elif Player1_value == 1 and Computer_value == 3:
+                player_wins += 1 
+                return "🥇Won by Player1🏆" 
+            elif Player1_value == 2 and Computer_value == 1:
+                player_wins += 1 
+                return "🥇Won by Player1🏆"
+            elif Player1_value == 3 and Computer_value == 2:
+                player_wins += 1
+                return "🥇Won by Player1🏆"
+            else:
+                python_wins += 1
+                return "🐍Won by Python🐍" 
+
+        game_result = decide_winner(Player1_value,Computer_value)
+        print(game_result)
+        
+        print("")
+        
+        nonlocal played_game_count
+        played_game_count += 1
+
+        print("Game Played Count : " + str(played_game_count))
+        print("\nPlayer wins : " + str(player_wins))
+        print("\nPython wins : " + str(python_wins))
 
         print(" ")
         
-        if last_value.lower() not in ["y","q"]:
-            continue          
+        print("Play again ? \nEnter y for Play again \nEnter q for Quit ")
+        
+        print(" ")
+
+        while True:
+            
+            last_value = input("y/q : ")
+
+            print(" ")
+            
+            if last_value.lower() not in ["y","q"]:
+                continue          
+            else:
+                break
+
+        print("")
+        
+        if last_value.lower() == "y" :
+            return rps_game()
+        
+        elif last_value.lower() == "q":
+            sys.exit("🙋‍♂️ Bye! 🙋‍♂️")
+        
         else:
-            break
+            print("Invalid value enter y or q correctly")
 
-    print("")
-    
-    if last_value.lower() == "y" :
-        return rps_game()
-    
-    elif last_value.lower() == "q":
-        sys.exit("🙋‍♂️ Bye! 🙋‍♂️")
-    
-    else:
-        print("Invalid value enter y or q correctly")
+    return rps_game
 
-rps_game()
+play = rps()
+play()
